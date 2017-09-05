f4b6ec04a88643cd4346f9f12b8579def908a0dc
dorimedini <dorimedini@gmail.com>
2017-09-05 17:57:41 +0300

    ...better now
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- TreeDecompositionEnumeration/src/dori_main.cpp
+++ TreeDecompositionEnumeration/src/dori_main.cpp
@@ -16,7 +16,6 @@ private:
         dsg.set_pmc_alg(PMCAlg(true, false, true)); // As fast as possible, synced
         dsg.set_ms_time_limit(max_seconds); // So we see time taken in the results
         dsg.set_pmc_time_limit(max_seconds);
-//        dsg.suppress_dump();                // No output to file yet
         for (GraphStats gs: dataset) {
             dsg.add_graph(gs.g, gs.text);
         }
@@ -44,20 +43,20 @@ public:
 
     // Construction merely populates the datasets
     DoriMain() :
-//        small_hard_stats(GraphProducer(true).add_random_pstep_range(utils__vector_range(25,35),0.2,0.45,0.1,instances).get()),
-//        sparse_stats(GraphProducer(true).add_random(utils__vector_range(50,60),{0.1},false,instances).get()),
-        dense_stats(GraphProducer(true).add_random_pstep_range(utils__vector_range(50,60),0.6,0.95,0.1,instances).get())
-//        ,bayesian_stats(GraphProducer(true).add_by_dir(DirectoryIterator(DATASET_NEW_DIR_BASE).skip("evid")).get())
+        small_hard_stats(GraphProducer(true).add_random_pstep_range(utils__vector_range(25,35),0.2,0.45,0.1,instances).get()),
+        sparse_stats(GraphProducer(true).add_random(utils__vector_range(50,60),{0.1},false,instances).get()),
+        dense_stats(GraphProducer(true).add_random_pstep_range(utils__vector_range(50,60),0.6,0.95,0.1,instances).get()),
+        bayesian_stats(GraphProducer(true).add_by_dir(DirectoryIterator(DATASET_NEW_DIR_BASE).skip("evid")).get())
         {}
 
     /**
      * Statistics generation.
      */
     DoriMain& calc_stats() {
-//        calc_stats_aux(small_hard_stats, small_hard_filename);
-//        calc_stats_aux(sparse_stats, sparse_filename);
+        calc_stats_aux(small_hard_stats, small_hard_filename);
+        calc_stats_aux(sparse_stats, sparse_filename);
         calc_stats_aux(dense_stats, dense_filename);
-//        calc_stats_aux(bayesian_stats, bayesian_filename);
+        calc_stats_aux(bayesian_stats, bayesian_filename);
         return *this;
     }
 
