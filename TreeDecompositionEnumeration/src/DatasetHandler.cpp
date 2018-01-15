9e3d5aec82cd2d10382745b2f58e5607384d93ab
dorimedini <dorimedini@gmail.com>
2018-01-16 00:30:01 +0200

    The real job to run
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- TreeDecompositionEnumeration/src/DatasetHandler.cpp
+++ TreeDecompositionEnumeration/src/DatasetHandler.cpp
@@ -1,16 +1,44 @@
 #include "DatasetHandler.h"
+#include "TestInterface.h"
+
+#define RESULTS_SUFFIX 0 // Change this to change the suffix of the target filename
 
 namespace tdenum {
 
-void DatasetHandler::dori_project_races() {
+void run_dataset(const vector<GraphStats>& vgs,
+                 const StatisticRequest& sr,
+                 const string& graph_dir,
+                 const string& dataset_name,
+                 const string& dataset_filename_prefix) {
+    vector<string> paths;
+    for (GraphStats gs: vgs) {
+        paths.push_back(graph_dir+"/"+gs.get_text());
+        gs.dump(paths.back());
+    }
+    TRACE(TRACE_LVL__ALWAYS, "Post-production. Now, " << graph_dir << " contains "
+                            << DirectoryIterator(graph_dir).file_count()
+                            << " files.");
 
-    const int generation = 0;   // Change this to change the suffix
-    Dataset dataset(DORI_PROJECT_RESULTS_DIR+"/SmallHard_"+UTILS__TO_STRING(generation));
-    StatisticRequest sr;
+    Dataset ds(DORI_PROJECT_RESULTS_DIR+"/"+dataset_filename_prefix+UTILS__TO_STRING(RESULTS_SUFFIX), paths);
+    ds.set_all_requests(sr);
+    TRACE(TRACE_LVL__ALWAYS, "Starting with " << dataset_name << " graphs.");
+    TRACE(TRACE_LVL__ALWAYS, "Reading from " << graph_dir << ", "
+                            << paths.size() << " files found.");
+    TRACE(TRACE_LVL__ALWAYS, "Calculating (this will take a while...)");
+    ds.calc();
+    TRACE(TRACE_LVL__ALWAYS, "Done. Dumping results to " << ds.get_file_path() << ", replacing previous contents");
+    ds.dump();
+}
+
+int DatasetHandler::dori_project_races() {
+
+    // Sanity
+    ASSERT(utils__mkdir(DORI_PROJECT_RESULTS_DIR));
 
     // Start by constructing the request.
     // We want a lot of data here, but the same data for each dataset which
     // makes things a little easier.
+    StatisticRequest sr;
     sr.reset()
       .set_all_algs_to_pmc_race()
       .set_count_ms()
@@ -26,127 +54,32 @@ void DatasetHandler::dori_project_races() {
     vector<GraphStats> vgs_smallhard = GraphProducer(DORI_PROJECT_DATASET_DIR_SMALLHARD,true)
             .add_random_pstep_range(utils__vector_range(25,35),0.2,0.45,0.1,10)
             .get();
-    vector<string> paths_smallhard;
-    for (auto gs: vgs_smallhard) {
-        paths_smallhard.push_back(DORI_PROJECT_DATASET_DIR_SMALLHARD+"/"+gs.get_text());
-        gs.dump(paths_smallhard.back());
-    }
-    TRACE(TRACE_LVL__ALWAYS, "Post-production. Now, " << DORI_PROJECT_DATASET_DIR_SMALLHARD << " contains "
-                            << DirectoryIterator(DORI_PROJECT_DATASET_DIR_SMALLHARD).file_count()
-                            << " files.");
-
-    Dataset dataset_smallhard(DORI_PROJECT_RESULTS_DIR+"/SmallHard_"+UTILS__TO_STRING(generation), paths_smallhard);
-    dataset_smallhard.set_all_requests(sr);
-    TRACE(TRACE_LVL__ALWAYS, "Starting with small hard graphs.");
-    TRACE(TRACE_LVL__ALWAYS, "Reading from " << DORI_PROJECT_DATASET_DIR_SMALLHARD << ", "
-                            << paths_smallhard.size() << " files found.");
-    TRACE(TRACE_LVL__ALWAYS, "Calculating (this will take a while...)");
-    dataset_smallhard.calc();
-    TRACE(TRACE_LVL__ALWAYS, "Done. Dumping results to " << dataset.get_file_path() << ", replacing previous contents");
-    dataset.dump();
-
-
-
-
-/*
-
-    Dataset dataset;
-    StatisticRequest sr;
-    const int generation = 0;   // Change this to change the suffix
-
-    // Start by constructing the request.
-    // We want a lot of data here, but the same data for each dataset which
-    // makes things a little easier.
-    sr.reset()
-      .set_all_algs_to_pmc_race()
-      .set_count_ms()
-      .set_count_pmc()
-      .set_time_limit_ms(5*60)        // Give MSs five minutes - if it takes more, then computing PMCs will take forever
-      .set_time_limit_pmc(20*60)      // 20 minutes for PMCs
-      .set_count_limit_ms(500000);    // More than enough. Memory may run out even with this limit
-
-    // Four datasets in this run.
-    TRACE(TRACE_LVL__ALWAYS, "=====START=====");
-
-    // Small & hard (see the produce() command for details)
-    dataset.set_input_dir(DORI_PROJECT_DATASET_DIR_SMALLHARD)
-            .set_output_dir(DORI_PROJECT_RESULTS_DIR)
-            .set_basename("SmallHard")
-            .set_suffix(UTILS__TO_STRING(generation));
-    TRACE(TRACE_LVL__ALWAYS, "Starting with small hard graphs.");
-    TRACE(TRACE_LVL__ALWAYS, "Reading from " << DORI_PROJECT_DATASET_DIR_SMALLHARD << ", "
-                            << DirectoryIterator(DORI_PROJECT_DATASET_DIR_SMALLHARD).file_count()
-                            << " files found.");
-    TRACE(TRACE_LVL__ALWAYS, "Loading graphs (first time requires production as well)...");
-    dataset.produce(GraphProducer(true).add_random_pstep_range(utils__vector_range(25,35),0.2,0.45,0.1,10).get());
-    TRACE(TRACE_LVL__ALWAYS, "Post-production. Now, " << DORI_PROJECT_DATASET_DIR_SMALLHARD << " contains "
-                            << DirectoryIterator(DORI_PROJECT_DATASET_DIR_SMALLHARD).file_count()
-                            << " files.");
-    TRACE(TRACE_LVL__ALWAYS, "Calculating (this will take a while...)");
-    dataset.calc(sr);
-    TRACE(TRACE_LVL__ALWAYS, "Done. Dumping results to " << dataset.file_path() << ", replacing previous contents");
-    dataset.dump(sr);
+    run_dataset(vgs_smallhard, sr, DORI_PROJECT_DATASET_DIR_SMALLHARD, "small hard", "SmallHard_");
 
     // Sparse graphs
-    dataset.reset()
-            .set_input_dir(DORI_PROJECT_DATASET_DIR_SPARSE)
-            .set_output_dir(DORI_PROJECT_RESULTS_DIR)
-            .set_basename("Sparse")
-            .set_suffix(UTILS__TO_STRING(generation));
-    TRACE(TRACE_LVL__ALWAYS, "Sparse graphs...");
-    TRACE(TRACE_LVL__ALWAYS, "Reading from " << DORI_PROJECT_DATASET_DIR_SPARSE << ", "
-                            << DirectoryIterator(DORI_PROJECT_DATASET_DIR_SPARSE).file_count()
-                            << " files found.");
-    TRACE(TRACE_LVL__ALWAYS, "Loading graphs (first time requires production as well)...");
-    dataset.produce(GraphProducer(true).add_random(utils__vector_range(50,60),{0.05},true,10).get());
-    TRACE(TRACE_LVL__ALWAYS, "Post-production. Now, " << DORI_PROJECT_DATASET_DIR_DENSE << " contains "
-                            << DirectoryIterator(DORI_PROJECT_DATASET_DIR_DENSE).file_count()
-                            << " files.");
-    TRACE(TRACE_LVL__ALWAYS, "Calculating (this will take a while...)");
-    dataset.calc(sr);
-    TRACE(TRACE_LVL__ALWAYS, "Done. Dumping results to " << dataset.file_path() << ", replacing previous contents");
-    dataset.dump(sr);
+    vector<GraphStats> vgs_sparse = GraphProducer(DORI_PROJECT_DATASET_DIR_SPARSE,true)
+            .add_dir(DORI_PROJECT_DATASET_DIR_SPARSE)
+            .add_random(utils__vector_range(50,60),{0.05},true,10)
+            .get();
+    run_dataset(vgs_sparse, sr, DORI_PROJECT_DATASET_DIR_SPARSE, "sparse", "Sparse_");
 
     // Dense graphs
-    dataset.reset()
-            .set_input_dir(DORI_PROJECT_DATASET_DIR_DENSE)
-            .set_output_dir(DORI_PROJECT_RESULTS_DIR)
-            .set_basename("Dense")
-            .set_suffix(UTILS__TO_STRING(generation));
-    TRACE(TRACE_LVL__ALWAYS, "Dense graphs...");
-    TRACE(TRACE_LVL__ALWAYS, "Reading from " << DORI_PROJECT_DATASET_DIR_DENSE << ", "
-                            << DirectoryIterator(DORI_PROJECT_DATASET_DIR_DENSE).file_count()
-                            << " files found.");
-    TRACE(TRACE_LVL__ALWAYS, "Loading graphs (first time requires production as well)...");
-    dataset.produce(GraphProducer(true).add_random_pstep_range(utils__vector_range(50,60),0.7,0.95,0.1,10).get());
-    TRACE(TRACE_LVL__ALWAYS, "Post-production. Now, " << DORI_PROJECT_DATASET_DIR_DENSE << " contains "
-                            << DirectoryIterator(DORI_PROJECT_DATASET_DIR_DENSE).file_count()
-                            << " files.");
-    TRACE(TRACE_LVL__ALWAYS, "Calculating (this will take a while...)");
-    dataset.calc(sr);
-    TRACE(TRACE_LVL__ALWAYS, "Done. Dumping results to " << dataset.file_path() << ", replacing previous contents");
-    dataset.dump(sr);
+    vector<GraphStats> vgs_dense = GraphProducer(DORI_PROJECT_DATASET_DIR_DENSE,true)
+            .add_dir(DORI_PROJECT_DATASET_DIR_DENSE)
+            .add_random_pstep_range(utils__vector_range(50,60),0.7,0.95,0.1,10)
+            .get();
+    run_dataset(vgs_dense, sr, DORI_PROJECT_DATASET_DIR_DENSE, "dense", "Dense_");
 
     // Bayesian graphs
-    dataset.reset()
-            .set_input_dir(DORI_PROJECT_DATASET_DIR_BAYESIAN)
-            .set_output_dir(DORI_PROJECT_RESULTS_DIR)
-            .set_basename("Bayesian")
-            .set_suffix(UTILS__TO_STRING(generation))
-            .set_filters({"evid"}); // Files in the Bayesian directories to be ignored
-    TRACE(TRACE_LVL__ALWAYS, "Bayesian graphs...");
-    TRACE(TRACE_LVL__ALWAYS, "Reading from " << DORI_PROJECT_DATASET_DIR_BAYESIAN << ", "
-                            << DirectoryIterator(DORI_PROJECT_DATASET_DIR_BAYESIAN).file_count()
-                            << " files found.");
-    TRACE(TRACE_LVL__ALWAYS, "Loading graphs...");
-    dataset.load();
-    TRACE(TRACE_LVL__ALWAYS, "Done. Calculating (this will take a while...)");
-    dataset.calc(sr);
-    TRACE(TRACE_LVL__ALWAYS, "Done. Dumping results to " << dataset.file_path() << ", replacing previous contents");
-    dataset.dump(sr);
+    vector<GraphStats> vgs_bayesian = GraphProducer(DORI_PROJECT_DATASET_DIR_BAYESIAN,true)
+            .add(DirectoryIterator(DORI_PROJECT_DATASET_DIR_BAYESIAN).skip("evid"))
+            .get();
+    run_dataset(vgs_bayesian, sr, DORI_PROJECT_DATASET_DIR_BAYESIAN, "bayesian", "Bayesian_");
 
     // Done.
-    TRACE(TRACE_LVL__ALWAYS, "=====END=====");*/
+    TRACE(TRACE_LVL__ALWAYS, "=====END=====");
+    return 0;
 }
 
-}
+
+} // namespace tdenum
--- TreeDecompositionEnumeration/src/DatasetHandler.h
+++ TreeDecompositionEnumeration/src/DatasetHandler.h
@@ -22,17 +22,17 @@ namespace tdenum {
  * Start by defining macros for target directories and files.
  */
 
+#define DORI_PROJECT_DATASET_DIR string(DATASET_DIR_BASE+"DoriProject"+string(1,SLASH))
 #define DORI_PROJECT_DATASET_DIR_SMALLHARD (DORI_PROJECT_DATASET_DIR + string("SmallHard") + string(1,SLASH))
 #define DORI_PROJECT_DATASET_DIR_SPARSE (DORI_PROJECT_DATASET_DIR + string("Sparse") + string(1,SLASH))
 #define DORI_PROJECT_DATASET_DIR_DENSE (DORI_PROJECT_DATASET_DIR + string("Dense") + string(1,SLASH))
 #define DORI_PROJECT_DATASET_DIR_BAYESIAN DATASET_NEW_DIR_BASE
 #define DORI_PROJECT_RESULTS_DIR string(RESULT_DIR_BASE+"DoriProject"+string(1,SLASH))
-#define DORI_PROJECT_DATASET_DIR string(DATASET_DIR_BASE+"DoriProject"+string(1,SLASH))
 
 class DatasetHandler {
 public:
 
-    void dori_project_races();
+    int dori_project_races();
 
 };
 
